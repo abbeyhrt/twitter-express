40b8be4a4fcab33686131e1da3585e572ca99d58
Abbey Hart <abbeyhrt@gmail.com>
2016-06-27 20:17:13 -0500

    Add routes for Tweets and a single Tweet

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const tweets = [];
 
 for (let i = 0; i < 20; i++) {
   tweets[i] = {
+    id: `${i + 1}`,
     body: faker.lorem.sentences(),
     author: {
       username: faker.internet.userName(),
@@ -16,11 +17,36 @@ for (let i = 0; i < 20; i++) {
   };
 }
 
+server.get('/tweets', function (req, res){
+  res.json(tweets);
+});
+
+//restful API
+// GET /tweets -> get all the tweets
+// tweets/1 => get all the tweets with an id of 1
+
+server.get('/tweets/:id', function (req, res){
+    const { id } = req.params;
+
+    const tweet = tweets.filter(function (tweet) {
+      if (tweet.id === id) {
+        return true
+      }
+
+      return false
+    });
+
+res.json(tweet[0]);
+
+});
+
+// GET tweets/new -> display a form for making a new tweet
+// POST /tweets with {tweets} -> display a form for posting tweets
+// GET tweets/edit -> display a form for editing a tweet
+// PUT tweets/1 with {tweet} edit a tweet with a body
+// DELETE /tweets/1 -> delete a tweet
 
 
-server.get('/', function (req, res){
-    res.json(tweets);
-} )
 
 server.listen(3000, function(){
   console.log('listening on http://localhost:3000');
